a4643f55dd11eca2ac97e93fe3b43099a76585d9
Arnaud Doko <arnauddoko@gmail.com>
2023-06-21 21:30:05 +0100

    new changes

---
+++ script.py
@@ -0,0 +1,10 @@
+### Using this file to follow the online workflow.
+
+### importing all items
+
+import streamlit as st
+from PIL import Image
+import numpy as np
+from model import predict
+import time
+import cv2 as cv
